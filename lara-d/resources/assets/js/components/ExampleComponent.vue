3e9c4b5054e2f4352737ccb7b290c953a6c23bc0
Masato <project-europa@excite.co.jp>
2018-01-31 10:53:05 +0900

    first-commit

---
+++ lara-d/resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr class="table-header">
+                        <th>ダウンロード</th>
+                        <th>オーナー名</th>
+                        <th>コメント</th>
+                        <th>ファイル名</th>
+                        <th>アップロード日時</th>
+                        <th>削除</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="file in files" :key="file.id">
+                        <td>{{ file.id }}</td>
+                        <td>{{ file.owner }}</td>
+                        <td>{{ file.comment }}</td>
+                        <td>{{ file.file_name }}</td>
+                        <td>{{ file.created_at }}</td>
+                        <td>{{ file.delete_flg }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        files: []
+      }
+    },
+    mounted() {
+      axios.get('http://localhost/test-data/test.json').then(res => {
+        this.files = res.data;
+      })
+    }
+  }
+</script>
---
+++ lara-d/resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- Styles -->
+    </head>
+    <body>
+        <main>
+            <div id="app">
+                <example-component></example-component>
+            </div>
+        </main>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
