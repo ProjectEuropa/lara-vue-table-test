b649341342efc937eb6cea21c49cac289a762633
Masato <project-europa@excite.co.jp>
2018-02-10 14:51:06 +0900

    feat:カスタムバリデーションCHEファイル判定バリデーションクラスを追加

--- lara-d/app/Providers/AppServiceProvider.php
+++ lara-d/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // CHEファイルバリデーション
+        Validator::extend('che_file', 'App\Validation\CustomValidator@validateCheFile');
     }
 
     /**
