d135ec121385ef93aca434b10d05280a39886880
M2 <project-europa@excite.co.jp>
2018-02-17 09:27:08 +0900

    feat:バリエーション発生時に値を保持する処理を追加

--- lara-d/resources/views/upload/index.blade.php
+++ lara-d/resources/views/upload/index.blade.php
@@ -21,28 +21,28 @@
                             <fieldset>
                                 <div class="form-group">
                                     <label for="teamOwnerName">オーナー名:</label>
-                                    <input type="text" name="teamOwnerName" class="form-control input-alternate" id="teamOwnerName" value="" style="padding: 0;">
+                                    <input type="text" name="teamOwnerName" class="form-control input-alternate" id="teamOwnerName" value="{{ strpos(Request::url(), 'simpleupload') !== false ? old('teamOwnerName') : Auth::user()->name }}" style="padding: 0;" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="teamComment">コメント:</label>
-                                    <textarea name="teamComment" class="form-control input-alternate" rows="10" id="teamComment" style="padding-bottom: 10em;"></textarea>
+                                    <textarea name="teamComment" class="form-control input-alternate" rows="10" id="teamComment" style="padding-bottom: 10em;" required>{{ old('teamComment') }}</textarea>
                                 </div>
                                 @if(strpos(Request::url(), 'simpleupload') !== false)
                                 <div class="form-group">
                                     <label for="teamDeletePassWord">削除パスワード:</label>
                                     <input type="text" name="teamDeletePassWord" class="form-control input-alternate" rows="5" id="teamDeletePassWord" value=""
-                                        style="padding: 0;">
+                                        style="padding: 0;" required>
                                 </div>
                                 @endif
                                 <div class="form-group">
                                     <label for="teamSearchTags">検索タグ:</label>
                                     <select id="teamSearchTags" class="form-control" name="teamSearchTags">
-                                        <option value="大会ゲスト許可">大会ゲスト許可</option>
-                                        <option value="フリーOKE">フリーOKE</option>
+                                        <option value="大会ゲスト許可" {{ (old('teamSearchTags') !== null && in_array('大会ゲスト許可', old('teamSearchTags'))) === true ? 'selected' : ''}}>大会ゲスト許可</option>
+                                        <option value="フリーOKE" {{ (old('teamSearchTags') !== null && in_array('フリーOKE', old('teamSearchTags'))) === true ? 'selected' : ''}}>フリーOKE</option>
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                    <input id="teamfile" type="file" style="display:none" name="teamFile">
+                                    <input id="teamfile" type="file" style="display:none" name="teamFile" required>
                                     <div class="input-group">
                                         <span class="input-group-btn">
                                             <button type="button" class="btn btn-info" onclick="$('input[id=teamfile]').click();">チームファイル</button>
@@ -72,29 +72,29 @@
                             <fieldset>
                                 <div class="form-group">
                                     <label for="matchOwnerName">オーナー名:</label>
-                                    <input type="text" name="matchOwnerName" class="form-control input-alternate" id="matchOwnerName" value="" style="padding: 0;">
+                                    <input type="text" name="matchOwnerName" class="form-control input-alternate" id="matchOwnerName" value="{{ strpos(Request::url(), 'simpleupload') !== false ? old('matchOwnerName') : Auth::user()->name }}" style="padding: 0;" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="matchComment">コメント:</label>
-                                    <textarea name="matchComment" class="form-control input-alternate" rows="5" id="matchComment" style="padding-bottom: 10em;"></textarea>
+                                    <textarea name="matchComment" class="form-control input-alternate" rows="5" id="matchComment" style="padding-bottom: 10em;" required>{{ old('matchComment') }}</textarea>
                                 </div>
                                 @if(strpos(Request::url(), 'simpleupload') !== false)
                                 <div class="form-group">
                                     <label for="matchDeletePassWord">削除パスワード:</label>
-                                    <input type="text" name="matchDeletePassWord" class="form-control input-alternate" rows="5" id="matchDeletePassWord" value="" style="padding: 0;">
+                                    <input type="text" name="matchDeletePassWord" class="form-control input-alternate" rows="5" id="matchDeletePassWord" value="" style="padding: 0;" required>
                                 </div>
                                 @endif
                                 <div class="form-group">
                                     <label for="matchSearchTags">検索タグ:</label>
                                     <select id="matchSearchTags" class="form-control" name="matchSearchTags">
-                                        <option value="ハーフリーグ">ハーフリーグ</option>
-                                        <option value="フルリーグ">フルリーグ</option>
-                                        <option value="トーナメント">トーナメント</option>
-                                        <option value="上級演習所">上級演習所</option>
+                                        <option value="ハーフリーグ" {{ (old('matchSearchTags') !== null && in_array('ハーフリーグ', old('matchSearchTags'))) === true ? 'selected' : ''}}>ハーフリーグ</option>
+                                        <option value="フルリーグ" {{ (old('matchSearchTags') !== null && in_array('フルリーグ', old('matchSearchTags'))) === true ? 'selected' : ''}}>フルリーグ</option>
+                                        <option value="トーナメント" {{ (old('matchSearchTags') !== null && in_array('トーナメント', old('matchSearchTags'))) === true ? 'selected' : ''}}>トーナメント</option>
+                                        <option value="上級演習所" {{ (old('matchSearchTags') !== null && in_array('上級演習所', old('matchSearchTags'))) === true ? 'selected' : ''}}>上級演習所</option>
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                        <input id="matchfile" type="file" style="display:none" name="matchFile">
+                                        <input id="matchfile" type="file" style="display:none" name="matchFile" required>
                                         <div class="input-group">
                                             <span class="input-group-btn">
                                                 <button type="button" class="btn btn-info" onclick="$('input[id=matchfile]').click();">マッチファイル</button>
