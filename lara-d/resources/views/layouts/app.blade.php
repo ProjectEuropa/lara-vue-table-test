644953d9b4ee9154dad49a83a06a52a8557b9c5a
M2 <project-europa@excite.co.jp>
2018-03-09 15:59:36 +0900

    feat: appの名称をEuropaに変更

--- lara-d/resources/views/layouts/app.blade.php
+++ lara-d/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Europa</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/material.css') }}" rel="stylesheet">
     <link href="{{ asset('css/top.css') }}" rel="stylesheet">
@@ -17,7 +17,6 @@
     @yield('css')
 </head>
 <body>
-    <body>
         <header>
             <nav class="navbar navbar-expand-lg">
                 <div class="container">
