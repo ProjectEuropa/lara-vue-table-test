6dc803ad494c42f2f3ae6bf6cc4efe6bc1b08fc5
M2 <project-europa@excite.co.jp>
2018-03-09 15:57:43 +0900

    feat: Apiをルーティング直書きからコントローラーで処理するように変更

--- lara-d/routes/api.php
+++ lara-d/routes/api.php
@@ -13,50 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('api')->get('/search/{searchtype}', function (Request $request, $searchtype) {
+Route::middleware('api')->get('/search/{searchType}', 'ApiController@search');
 
-    $files = DB::table('files')
-            ->select('id', 'upload_owner_name', 'file_name', 'file_comment', 'created_at', 'upload_user_id', 'upload_type',  'search_tag1', 'search_tag2', 'search_tag3', 'search_tag4');
-
-    if ($searchtype == 'team') {
-        $files = $files->where('data_type', '=', '1');
-    } else {
-        $files = $files->where('data_type', '=', '2');
-    }
-
-    if ($request->ordertype) {
-        $files = $files->orderby('id', $request->ordertype);
-    } else {
-        $files = $files->orderby('id', 'desc');
-    }
-
-    if ($request->keyword) {
-        $files = $files->where('file_comment', 'LIKE', '%'.$request->keyword.'%');
-    }
-
-    return $files->paginate(10);
-});
-
-Route::middleware('api')->get('/sumdownload/{searchtype}', function (Request $request, $searchtype) {
-
-    $files = DB::table('files')
-            ->select('id', 'upload_owner_name', 'file_name', 'file_comment', 'created_at', 'upload_user_id', 'search_tag1', 'search_tag2', 'search_tag3', 'search_tag4');
-
-    if ($searchtype == 'team') {
-        $files = $files->where('data_type', '=', '1');
-    } else {
-        $files = $files->where('data_type', '=', '2');
-    }
-
-    if ($request->ordertype) {
-        $files = $files->orderby('id', $request->ordertype);
-    } else {
-        $files = $files->orderby('id', 'desc');
-    }
-
-    if ($request->keyword) {
-        $files = $files->where('file_comment', 'LIKE', '%'.$request->keyword.'%');
-    }
-
-    return $files->paginate(50);
-});
+Route::middleware('api')->get('/sumdownload/{searchType}', 'ApiController@sumdownload');
